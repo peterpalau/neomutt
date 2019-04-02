963f38e80580e917bc78a15e373a7fb4175d59e8
Richard Russon <rich@flatcap.org>
2019-04-02 12:50:16 +0000

    fix trash_append()
    Prevent a crash when reopening a Mailbox.
    
    > How exactly do these changes prevent the crash from occurring?
    
    It's all about how mx_mbox_open() handles flags.  These four examples
    are where we have a Mailbox open in 'normal' mode and try to open it
    again in 'append' mode.
    
    'Append' mode only makes sense for 'mbox' and 'compress', but the flag
    affects mx_mbox_open(). It causes the function to create a duplicate
    Mailbox, which is then used and discarded. The original Mailbox is now
    out of sync.
    
    The workaround, simply hides the flag for mx_mbox_open(), but
    temporarily sets the internal bool append. This means that we work with
    one Mailbox.

--- mx.c
+++ mx.c
@@ -490,9 +490,12 @@ static int trash_append(struct Mailbox *m)
 #endif
 
   struct Mailbox *m_trash = mx_path_resolve(C_Trash);
-  struct Context *ctx_trash = mx_mbox_open(m_trash, MUTT_APPEND);
+  struct Context *ctx_trash = mx_mbox_open(m_trash, MUTT_OPEN_NO_FLAGS);
   if (ctx_trash)
   {
+    bool old_append = m_trash->append;
+    m_trash->append = true;
+
     /* continue from initial scan above */
     for (int i = first_del; i < m->msg_count; i++)
     {
@@ -500,12 +503,14 @@ static int trash_append(struct Mailbox *m)
       {
         if (mutt_append_message(ctx_trash->mailbox, m, m->emails[i], 0, 0) == -1)
         {
+          m_trash->append = old_append;
           mx_mbox_close(&ctx_trash);
           return -1;
         }
       }
     }
 
+    m_trash->append = old_append;
     mx_mbox_close(&ctx_trash);
   }
   else
